5944decd5da577f6c2f700dd07a796f7785fba7b
David Griver <dgriver@gmail.com>
2016-09-05 14:50:56 +0300

    add rest api with django_rest

---
+++ moonshot/books/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from rest_framework import routers
+from .views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = router.urls
